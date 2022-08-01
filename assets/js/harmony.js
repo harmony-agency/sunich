e8104012724beda9f34977f4f8952edcc8621c60
Vahideh-Asemi <v.as74@yahoo.com>
2022-08-01 12:04:12 +0430

    commit v11

--- assets/js/harmony.js
+++ assets/js/harmony.js
@@ -1,26 +1,26 @@
 // Aos animation
 AOS.init();
 
-if(window.matchMedia("(max-width: 767px)").matches){
-
-
-  $.fn.animateImages = function(angle, duration, easing, complete) {
-    return this.each(function() {
+if (window.matchMedia("(max-width: 767px)").matches) {
+  $.fn.animateImages = function (angle, duration, easing, complete) {
+    return this.each(function () {
       var $elem = $(this);
-  
-      $({deg: 0}).animate({deg: angle}, {
-        duration: duration,
-        easing: easing,
-        step: function(now) {
-          $elem.css({
-             opacity: 0 + now
-           });
-        },
-        complete: complete || $.noop
-      });
+
+      $({ deg: 0 }).animate(
+        { deg: angle },
+        {
+          duration: duration,
+          easing: easing,
+          step: function (now) {
+            $elem.css({
+              opacity: 0 + now,
+            });
+          },
+          complete: complete || $.noop,
+        }
+      );
     });
   };
-
 
   var swiper = new Swiper("#home-carousel", {
     slidesPerView: 2.5,
@@ -43,82 +43,73 @@ if(window.matchMedia("(max-width: 767px)").matches){
     slidesPerGroup: 1,
     loop: true,
     loopFillGroupWithBlank: true,
-    autoplay: {
-      delay: 2500,
-    },
+    // autoplay: {
+    //   delay: 2500,
+    // },
     navigation: {
       nextEl: ".swiper-next",
       prevEl: ".swiper-prev",
     },
   });
 
-
-  $("#product-carousel a").click(function(){
-
-    let htmlContent = $(this).data('content');
-    let folder = $(this).data('folder');
-
-
-    $("#productModal .product-content").load('product/'+ folder + '/' +htmlContent+'.html');
-    
+  var myModalEl = document.getElementById("productModal");
+  myModalEl.addEventListener("shown.bs.modal", function (event) {
+    // do something...
+    console.log("modal");
+    productSwiper.destroy();
+    productSwiper.init("#product-carousel");
   });
 
+
+
   
+  $("#product-carousel a").click(function () {
+    let htmlContent = $(this).data("content");
+    let folder = $(this).data("folder");
+    $("#productModal .product-content").load(
+      "product/" + folder + "/" + htmlContent + ".html"
+    );
+  });
 
   // $('.card-img img').animateImages(1);
-
-}else{
-  
-
+} else {
   var antioxidanLottie = bodymovin.loadAnimation({
-      // animationData: { /* ... */ },
-      container: document.getElementById("antioxidanLottie"), // required
-      path: "antioxidanLottie.json", // required
-      renderer: "svg", // required
-      autoplay: true, // optional
-      loop: false, // Optional
+    // animationData: { /* ... */ },
+    container: document.getElementById("antioxidanLottie"), // required
+    path: "antioxidanLottie.json", // required
+    renderer: "svg", // required
+    autoplay: true, // optional
+    loop: false, // Optional
   });
   var vitamineLottie = bodymovin.loadAnimation({
-      // animationData: { /* ... */ },
-      container: document.getElementById("vitamineLottie"), // required
-      path: "vitamineLottie.json", // required
-      renderer: "svg", // required
-      autoplay: true, // optional
-      loop: false, // Optional
-
+    // animationData: { /* ... */ },
+    container: document.getElementById("vitamineLottie"), // required
+    path: "vitamineLottie.json", // required
+    renderer: "svg", // required
+    autoplay: true, // optional
+    loop: false, // Optional
   });
   var nfcLottie = bodymovin.loadAnimation({
-      // animationData: { /* ... */ },
-      container: document.getElementById("nfcLottie"), // required
-      path: "nfcLottie.json", // required
-      renderer: "svg", // required
-      autoplay: true, // optional
-      loop: false, // Optional
+    // animationData: { /* ... */ },
+    container: document.getElementById("nfcLottie"), // required
+    path: "nfcLottie.json", // required
+    renderer: "svg", // required
+    autoplay: true, // optional
+    loop: false, // Optional
   });
-
-
 }
 
+var windowHeight = $(window).height();
 
-
-
-
-  var windowHeight = $(window).height();
-
-  $(document).ready(function () {
-    $(".main-box").css("height", $(window).height() - 140);
-    $(".box-img").css("height", $(window).height() * 0.5);
-  });
-  $(window).resize(function () {
-    $(".main-box").css("height", $(window).height() - 140);
-    $(".box-img").css("height", $(window).height() * 0.5);
-  });
-
-
-$('#backHome').click(function(){
-
-  window.location.href = '/';
-
-
+$(document).ready(function () {
+  $(".main-box").css("height", $(window).height() - 140);
+  $(".box-img").css("height", $(window).height() * 0.5);
 });
-  
+$(window).resize(function () {
+  $(".main-box").css("height", $(window).height() - 140);
+  $(".box-img").css("height", $(window).height() * 0.5);
+});
+
+$("#backHome").click(function () {
+  window.location.href = "/";
+});
--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,48 +1,44 @@
+const currentURL = $(location).attr("href");
 
+if (window.matchMedia("(max-width: 767px)").matches) {
+  let address = currentURL.substring(currentURL.lastIndexOf("/") + 1);
 
-
- const currentURL = $(location).attr("href");
-
-
-
-if(window.matchMedia("(max-width: 767px)").matches){
-
-    let address = currentURL.substring(currentURL.lastIndexOf('/') + 1);
-
-    if (address == "antioxidan.html") {
+  if (address == "antioxidan.html") {
     $("#anti-preload").trigger("play");
-    $('#anti-preload').show();
-    }
-    if (address == "vitamine.html") {
+    $("#anti-preload").show();
+  }
+  if (address == "vitamine.html") {
     $("#vitamine-preload").trigger("play");
-    $('#vitamine-preload').show();
-    }
-    if (address == "nfc.html") {
+    $("#vitamine-preload").show();
+  }
+  if (address == "nfc.html") {
     $("#nfc-preload").trigger("play");
-    $('#nfc-preload').show();
-    }
+    $("#nfc-preload").show();
+  }
 
-    setTimeout(function () {
+  setTimeout(function () {
     $("#preloader").fadeOut();
-    }, 2000);
-}else{
+  }, 2000);
+} else {
+  // preloader
+  let sectionId = currentURL.split("/");
+  if (
+    sectionId[1] == "antioxidan-part" ||
+    sectionId[1] == "antioxidanSection"
+  ) {
+    $("#anti-preload").trigger("play");
+    $("#anti-preload").show();
+  }
+  if (sectionId[1] == "vitamine-part" || sectionId[1] == "vitamineSection") {
+    $("#vitamine-preload").trigger("play");
+    $("#vitamine-preload").show();
+  }
+  if (sectionId[1] == "nfc-part" || sectionId[1] == "nfcSection") {
+    $("#nfc-preload").trigger("play");
+    $("#nfc-preload").show();
+  }
 
-        // preloader
-        let sectionId = currentURL.split("/");
-    if (sectionId[1] == "antioxidan-part" || sectionId[1] == "antioxidanSection") {
-        $("#anti-preload").trigger("play");
-        $('#anti-preload').show();
-        }
-        if (sectionId[1] == "vitamine-part" || sectionId[1] == "vitamineSection") {
-        $("#vitamine-preload").trigger("play");
-        $('#vitamine-preload').show();
-        }
-        if (sectionId[1] == "nfc-part" || sectionId[1] == "nfcSection") {
-        $("#nfc-preload").trigger("play");
-        $('#nfc-preload').show();
-        }
-    
-        setTimeout(function () {
-        $("#preloader").fadeOut();
-        }, 2000);
+  setTimeout(function () {
+    $("#preloader").fadeOut();
+  }, 2000);
 }
