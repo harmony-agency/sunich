c196ecee59758357ef056655b2f5ac3601256fc8
Vahideh-Asemi <v.as74@yahoo.com>
2022-08-09 16:21:57 +0430

    commit v15

--- assets/js/product.js
+++ assets/js/product.js
@@ -7,49 +7,80 @@ var myFullpage = new fullpage("#fullpage", {
   loopBottom: true,
 });
 
-$.fn.animateRotate = function(angle, duration, easing, complete) {
-  return this.each(function() {
+$.fn.animateRotate = function (angle, duration, easing, complete) {
+  return this.each(function () {
     var $elem = $(this);
 
-    $({deg: 0}).animate({deg: angle}, {
-      duration: duration,
-      easing: easing,
-      step: function(now) {
-        $elem.css({
-          position: 'relative',
-          bottom: now,
-           transform: 'rotate(' + now + 'deg)'
-         });
-      },
-      complete: complete || $.noop
-    });
+    $({ deg: 0 }).animate(
+      { deg: angle },
+      {
+        duration: duration,
+        easing: easing,
+        step: function (now) {
+          $elem.css({
+            position: "relative",
+            bottom: now,
+            transform: "rotate(" + now + "deg)",
+          });
+        },
+        complete: complete || $.noop,
+      }
+    );
   });
 };
 
+// anti-splash
+$("#antioxidanProducts .box-item a").hover(
+  function () {
+    $(this).children(".normal-img").animateRotate(20, 5);
+    $(this)
+      .children(".normal-img")
+      .css("background-image", "url(assets/images/anti-splash.png)");
+  },
+  function () {
+    $(this).children(".normal-img").animateRotate(0);
+    $(this).children(".normal-img").css("background-image", "unset");
+  }
+);
+// vitamine-splash
+$("#vitamineProducts .box-item a").hover(
+  function () {
+    $(this).children(".normal-img").animateRotate(20, 5);
+    $(this)
+      .children(".normal-img")
+      .css("background-image", "url(assets/images/vitamin-splash.png)");
+  },
+  function () {
+    $(this).children(".normal-img").animateRotate(0);
+    $(this).children(".normal-img").css("background-image", "unset");
+  }
+);
 
-$(".box-item a").hover(function(){
-    $(this).children('.normal-img').animateRotate(20,5);
-    $(this).children('.normal-img').css('background-image', 'url(assets/images/product-splash.png)');
+// nfc-splash
+$("#nfcProducts .box-item a").hover(
+  function () {
+    $(this).children(".normal-img").animateRotate(20, 5);
+    $(this)
+      .children(".normal-img")
+      .css("background-image", "url(assets/images/nfc-splash.png)");
+  },
+  function () {
+    $(this).children(".normal-img").animateRotate(0);
+    $(this).children(".normal-img").css("background-image", "unset");
+  }
+);
 
-    }, function(){
-      $(this).children('.normal-img').animateRotate(0);
-      $(this).children('.normal-img').css('background-image', 'unset');
-
-    });
-
-
-
-    var swiper = new Swiper("#product-carousel", {
-      slidesPerView: 1.5,
-      spaceBetween: 5,
-      slidesPerGroup: 1,
-      // loop: true,
-      loopFillGroupWithBlank: true,
-      autoplay: {
-        delay: 2500,
-      },
-      navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-      },
-    });
+var swiper = new Swiper("#product-carousel", {
+  slidesPerView: 1.5,
+  spaceBetween: 5,
+  slidesPerGroup: 1,
+  // loop: true,
+  loopFillGroupWithBlank: true,
+  autoplay: {
+    delay: 2500,
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
